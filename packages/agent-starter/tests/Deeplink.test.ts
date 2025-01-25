bda2004a6751894f742dd08706601ef0aaa8c53a
fabri <fguespe@gmail.com>
2025-01-25 18:51:10 -0500

    test

--- packages/agent-starter/tests/Deeplink.test.ts
+++ packages/agent-starter/tests/Deeplink.test.ts
@@ -5,7 +5,7 @@ describe("Deeplink Tests", () => {
   test("If no dm created, create one", async () => {
     const xmtp = new XMTP();
     await xmtp.init();
-    console.log("address", xmtp.address);
+
     if (!xmtp.address) {
       expect(xmtp.address).toBeUndefined();
       return;
@@ -15,12 +15,11 @@ describe("Deeplink Tests", () => {
       expect(inboxId).toBeUndefined();
       return;
     }
-    console.log("inboxId", inboxId);
+
     let dm = xmtp.client?.conversations.getDmByInboxId(inboxId);
     console.log("dm", dm);
     if (!dm) {
       const dmGroup = await xmtp.client?.conversations.newDm(xmtp.address);
-      console.log("dmGroup", dmGroup);
       dm = dmGroup;
     }
     console.log("dm", dm?.id);
