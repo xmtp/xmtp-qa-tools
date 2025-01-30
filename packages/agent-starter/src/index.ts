62b541e2c83f67e9eba161591c5b25ab6155e703
Ry Racherbaumer <ry@xmtp.com>
2025-01-29 22:01:59 -0600

    Remove packages

--- packages/agent-starter/src/index.ts
+++
@@ -1,3 +0,0 @@
-export * from "./lib/xmtp.js";
-export type * from "./lib/types.js";
-export * from "./content-types/agent-message.js";
--- packages/lookup/CHANGELOG.md
+++
@@ -1,7 +0,0 @@
-# @xmtp/lookup
-
-## 0.0.4
-
-### Patch Changes
-
-- d8fd67f: Fixed various type and code issues
