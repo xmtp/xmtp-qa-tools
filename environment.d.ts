3fafb23970f7158c3944135eef233b1a2d9248a9
Fabri <fguespe@gmail.com>
2025-01-31 14:41:51 -0300

    GPT Readme (#16)
    * readme
    
    * polished readme
    
    * fix copy

--- environment.d.ts
+++ environment.d.ts
@@ -3,6 +3,7 @@ declare global {
     interface ProcessEnv {
       WALLET_KEY: `0x${string}` | undefined;
       ENCRYPTION_KEY: string | undefined;
+      OPENAI_API_KEY: string | undefined;
     }
   }
 }
