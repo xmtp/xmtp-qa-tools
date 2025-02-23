f19e698d81b28456e9ab20ae3fbf1865aea5a46c
fabri <fguespe@gmail.com>
2025-02-23 17:39:30 -0300

    added build command

--- scripts/reflect.ts
+++ scripts/reflect.ts
@@ -1,29 +1,22 @@
 import { type XmtpEnv } from "@xmtp/node-sdk";
-import { getXmtpClient } from "../helpers/client";
-import { ReflectTestSuite } from "../helpers/reflect";
-import { streamMessages } from "../helpers/testing";
-
-const reflectTestSuite = new ReflectTestSuite();
 
 const env: XmtpEnv = "dev";
 
 async function main() {
-  console.log("Syncing conversations...");
-  const client = await getXmtpClient("bob", env);
-
-  console.log(
-    `Agent initialized on ${client.accountAddress}\nSend a message on http://xmtp.chat/dm/${client.accountAddress}?env=${env}`,
-  );
-
-  // Start the message listener in the background.
-  streamMessages(client).catch(console.error);
-
-  // Run the GM test and wait for its execution ID.
-  const { executionId } = await reflectTestSuite.runSendingGmTest();
-
-  if (executionId) {
-    await reflectTestSuite.pollExecutionStatus(reflectTestSuite, executionId);
-  }
+  // console.log("Syncing conversations...");
+  // const client = await Client.create(signer, encryptionKey, {
+  //   env,
+  // });
+  // console.log(
+  //   `Agent initialized on ${client.accountAddress}\nSend a message on http://xmtp.chat/dm/${client.accountAddress}?env=${env}`,
+  // );
+  // // Start the message listener in the background.
+  // streamMessages(client).catch(console.error);
+  // // Run the GM test and wait for its execution ID.
+  // const { executionId } = await reflectTestSuite.runSendingGmTest();
+  // if (executionId) {
+  //   await reflectTestSuite.pollExecutionStatus(reflectTestSuite, executionId);
+  // }
 }
 
 main().catch(console.error);
