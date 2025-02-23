48fa7168d26da750b61b26bf8226572cd7d1c05d
fabri <fguespe@gmail.com>
2025-02-23 18:03:06 -0300

    fix vitest config

--- vitest.config.ts
+++ vitest.config.ts
@@ -10,7 +10,6 @@ export default defineConfig({
     api: {
       host: "0.0.0.0", // Bind to all interfaces
       port: 51204, // Use Railway's assigned port or default
-      allowedHosts: ["qa-testing-production.up.railway.app"],
     },
   },
 });
