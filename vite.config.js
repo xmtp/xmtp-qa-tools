97953b94ee9b1e16f60fae2b8f890d681dfb35fe
fabri <fguespe@gmail.com>
2025-02-24 09:18:21 -0300

    base vite

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  base: "/__vitest__/#/",
   server: {
     host: true,
     port: 51204, // Use Railway's assigned port or default
