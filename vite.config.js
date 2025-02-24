e7cf53551a58dbe2963bd089e0d8dff26d984e2a
fabri <fguespe@gmail.com>
2025-02-24 09:15:25 -0300

    Vite config

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,7 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  title: "QA Testing",
-  icon: "./public/favicon.ico",
   server: {
-    host: true,
-    port: 51204, // Use Railway's assigned port or default
-    allowedHosts: true,
-  },
-  api: {
     host: true,
     port: 51204, // Use Railway's assigned port or default
     allowedHosts: true,
