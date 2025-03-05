3512086972e4298db7fa33896441e444c372e418
fabri <fguespe@gmail.com>
2025-03-05 16:34:49 -0300

    bug

--- bugs/bug_panic/test.test.ts
+++ bugs/bug_panic/test.test.ts
@@ -9,21 +9,16 @@ loadEnv(testName);
 describe(testName, () => {
   let personas: Record<string, Persona>;
 
-  beforeAll(async () => {
-    personas = await getWorkers(20, testName);
-  });
-  afterAll(async () => {
-    await closeEnv(testName, personas);
-  });
-
   it("createGroupByInboxIds: should measure creating a group with inbox ids", async () => {
+    personas = await getWorkers(30, testName);
+    const workerArray = Object.values(personas);
     const groupByInboxIds =
       await personas.bob.client!.conversations.newGroupByInboxIds(
         Object.values(personas).map((persona) => persona.client!.inboxId),
       );
-    // for (const worker of workerArray) {
-    //   await worker.worker?.terminate();
-    // }
+    for (const worker of workerArray) {
+      await worker.worker?.terminate();
+    }
     expect(groupByInboxIds.id).toBeDefined();
   });
 });
