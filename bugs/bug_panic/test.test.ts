8d3e8409bb1dd965c77edc602ae08325df75c7e7
fabri <fguespe@gmail.com>
2025-03-05 16:35:04 -0300

    bug

--- bugs/bug_panic/test.test.ts
+++ bugs/bug_panic/test.test.ts
@@ -1,16 +1,13 @@
-import { closeEnv, loadEnv } from "@helpers/client";
-import type { Persona } from "@helpers/types";
+import { loadEnv } from "@helpers/client";
 import { getWorkers } from "@helpers/workers/factory";
-import { afterAll, beforeAll, describe, expect, it } from "vitest";
+import { describe, expect, it } from "vitest";
 
 const testName = "bug_panic";
 loadEnv(testName);
 
 describe(testName, () => {
-  let personas: Record<string, Persona>;
-
   it("createGroupByInboxIds: should measure creating a group with inbox ids", async () => {
-    personas = await getWorkers(30, testName);
+    const personas = await getWorkers(30, testName);
     const workerArray = Object.values(personas);
     const groupByInboxIds =
       await personas.bob.client!.conversations.newGroupByInboxIds(
