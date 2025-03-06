9eaafc87b193b48aea84f4c071103dffc051cc44
fabri <fguespe@gmail.com>
2025-03-06 20:36:58 -0300

    added times to gm bot test

--- tests/TS_Gm.test.ts
+++ tests/TS_Gm.test.ts
@@ -24,8 +24,10 @@ describe(testName, () => {
   let start: number;
 
   beforeAll(async () => {
+    console.time("beforeAll");
     fs.rmSync(".data", { recursive: true, force: true });
     personas = await getWorkers(["bob"], testName);
+    console.timeEnd("beforeAll");
   });
 
   beforeEach(() => {
@@ -35,7 +37,9 @@ describe(testName, () => {
   });
 
   afterAll(async () => {
+    console.time("afterAll");
     await closeEnv(testName, personas);
+    console.timeEnd("afterAll");
   });
 
   afterEach(function () {
@@ -51,25 +55,47 @@ describe(testName, () => {
   });
 
   it("gm-bot: should measure sending a gm", async () => {
+    console.time("gm-bot-test");
+
+    console.time("create-conversation");
     convo = await personas.bob.client!.conversations.newDm(gmBotAddress);
+    console.timeEnd("create-conversation");
+
+    console.time("sync-conversation");
     await convo.sync();
+    console.timeEnd("sync-conversation");
+
+    console.time("get-messages");
     const messages = await convo.messages();
+    console.timeEnd("get-messages");
+
     console.log("Messages before sending", messages.length);
     // We'll expect this random message to appear in Joe's stream
     const message = "gm-" + Math.random().toString(36).substring(2, 15);
 
+    console.time("send-message");
     const dmId = await convo.send(message);
+    console.timeEnd("send-message");
 
+    console.time("wait-for-response");
     await new Promise((resolve) => setTimeout(resolve, 2000));
+    console.timeEnd("wait-for-response");
+
+    console.time("get-messages-after");
     const messagesAfter = await convo.messages();
+    console.timeEnd("get-messages-after");
+
     expect(messagesAfter.length).toBe(messages.length + 2);
     console.log("Messages after sending", messagesAfter.length);
 
     expect(dmId).toBeDefined();
+    console.timeEnd("gm-bot-test");
   });
 
   it("should respond to a message", async () => {
+    console.time("respond-to-message-test");
     const result = await testGmBot(gmBotAddress);
     expect(result).toBe(true);
+    console.timeEnd("respond-to-message-test");
   });
 });
