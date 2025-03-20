6a1d1b4ca76ac7c876e58cc42daebc32e4f7ac3e
fabri <fguespe@gmail.com>
2025-03-20 13:33:05 -0300

    can message

--- tests/clients.test.ts
+++ tests/clients.test.ts
@@ -1,58 +1,55 @@
 import fs from "fs";
-import { closeEnv, loadEnv } from "@helpers/client";
-import { type WorkerManager } from "@helpers/types";
-import { getDataSubFolderCount, getWorkers } from "@workers/manager";
-import { afterAll, beforeAll, describe, expect, it } from "vitest";
+import { loadEnv } from "@helpers/client";
+import { IdentifierKind } from "@helpers/types";
+import { getWorkers } from "@workers/manager";
+import { beforeAll, describe, expect, it } from "vitest";
 
 const testName = "clients";
 loadEnv(testName);
 
 describe(testName, () => {
-  let workers: WorkerManager;
-
-  let folderCount: number = 0;
   beforeAll(() => {
     fs.rmSync(".data", { recursive: true, force: true });
   });
 
-  afterAll(async () => {
-    await closeEnv(testName, workers);
+  it("canMessage: should measure canMessage", async () => {
+    const workers = await getWorkers(["henry"], testName, "none");
+    const address = "0x83e894b586d183380e1fb6602cd8349520c03dfa";
+    const canMessage = await workers.get("henry")!.client.canMessage([
+      {
+        identifier: address,
+        identifierKind: IdentifierKind.Ethereum,
+      },
+    ]);
+    console.log(canMessage.get(address));
+    expect(canMessage.get(address)).toBe(true);
   });
 
   it("create random workers", async () => {
-    workers = await getWorkers(["random"], testName, "none");
-    folderCount++;
+    const workers = await getWorkers(["random"], testName, "none");
     expect(workers.get("random")?.client?.inboxId).toBeDefined();
-    expect(getDataSubFolderCount()).toBe(folderCount);
   });
 
   it("should create a worker", async () => {
-    workers = await getWorkers(["bob", "random"], testName, "none");
-    folderCount++;
+    const workers = await getWorkers(["bob", "random"], testName, "none");
     expect(workers.get("bob")?.client?.inboxId).toBeDefined();
-    expect(getDataSubFolderCount()).toBe(folderCount);
   });
 
   it("should create a random worker", async () => {
-    workers = await getWorkers(["random"], testName, "none");
+    const workers = await getWorkers(["random"], testName, "none");
 
     expect(workers.get("random")?.client?.inboxId).toBeDefined();
-    expect(getDataSubFolderCount()).toBe(folderCount);
   });
 
   it("should create multiple workers", async () => {
-    workers = await getWorkers(
+    const workers = await getWorkers(
       ["bob", "alice", "randompep", "randombob"],
       testName,
       "none",
     );
-    folderCount++;
-    folderCount++;
-    folderCount++;
     expect(workers.get("bob")?.client?.inboxId).toBeDefined();
     expect(workers.get("alice")?.client?.inboxId).toBeDefined();
     expect(workers.get("randompep")?.client?.inboxId).toBeDefined();
     expect(workers.get("randombob")?.client?.inboxId).toBeDefined();
-    expect(getDataSubFolderCount()).toBe(folderCount);
   });
 });
