1e8e5106a4293fb206e509c31e21915b45260a2d
Fabri <fguespe@gmail.com>
2025-04-29 17:36:40 -0400

    Add logging level parameter to regression client function (#186)
    * Add logging level parameter to regression client function
    
    * Refactor logging for clarity and add inbox tracking
    
    * Remove unused import from test suite for clarity
    
    ---------
    
    Co-authored-by: fabrizio guespe <fabrizioguespe@FABRIZIOs-MacBook-Pro.local>

--- helpers/client.ts
+++ helpers/client.ts
@@ -117,6 +117,7 @@ export const regressionClient = async (
       dbEncryptionKey,
       dbPath,
       env,
+      loggingLevel,
     });
     libXmtpVersionAfterClient = getLibXmtpVersion(ClientClass);
   } else {
