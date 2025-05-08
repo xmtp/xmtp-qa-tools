8b972f2cc132257c17f3ecb7bdfa60800d7d272f
fabrizio guespe <fabrizioguespe@Fabri-M3.local>
2025-05-08 00:05:07 -0300

    Refactor group performance tests for improved accuracy

--- suites/TS_GroupPerformance/TS_GroupPerformance.test.ts
+++ suites/TS_GroupPerformance/TS_GroupPerformance.test.ts
@@ -1,9 +1,9 @@
 import { loadEnv } from "@helpers/client";
 import generatedInboxes from "@helpers/generated-inboxes.json";
 import { logError } from "@helpers/logger";
-import { verifyMessageStream } from "@helpers/streams";
+import { verifyGroupUpdateStream, verifyMessageStream } from "@helpers/streams";
 import { setupTestLifecycle } from "@helpers/vitest";
-import { typeOfResponse, typeofStream } from "@workers/main";
+import { typeofStream } from "@workers/main";
 import { getWorkers, type WorkerManager } from "@workers/manager";
 import { type Conversation, type Group } from "@xmtp/node-sdk";
 import { describe, expect, it } from "vitest";
@@ -12,19 +12,14 @@ const testName = "ts_group_performance";
 loadEnv(testName);
 
 describe(testName, async () => {
-  const batchSize = 100;
-  const total = 400;
+  const batchSize = 50;
+  const total = 100;
   let workers: WorkerManager;
   let start: number;
   let hasFailures: boolean = false;
   let testStart: number;
 
-  workers = await getWorkers(
-    10,
-    testName,
-    typeofStream.Message,
-    typeOfResponse.None,
-  );
+  workers = await getWorkers(10, testName, typeofStream.None);
 
   setupTestLifecycle({
     expect,
@@ -71,10 +66,37 @@ describe(testName, async () => {
 
     it(`receiveLargeGroupMessage-${i}: should create a group and measure all streams`, async () => {
       try {
+        workers = await getWorkers(10, testName, typeofStream.Message);
         const verifyResult = await verifyMessageStream(
           newGroup,
           workers.getWorkers(),
+          1,
+          undefined,
+          undefined,
+          () => {
+            console.log("Message sent, starting timer now");
+            start = performance.now();
+          },
         );
+        console.log("verifyResult", JSON.stringify(verifyResult));
+        expect(verifyResult.messages.length).toEqual(
+          workers.getWorkers().length - 1,
+        );
+        expect(verifyResult.allReceived).toBe(true);
+      } catch (e) {
+        hasFailures = logError(e, expect.getState().currentTestName);
+        throw e;
+      }
+    });
+
+    it("verifyLargeGroupMetadataStream: should update group name", async () => {
+      try {
+        workers = await getWorkers(10, testName, typeofStream.GroupUpdated);
+        const verifyResult = await verifyGroupUpdateStream(
+          newGroup as Group,
+          workers.getWorkers(),
+        );
+
         console.log("verifyResult", JSON.stringify(verifyResult));
         expect(verifyResult.messages.length).toEqual(
           workers.getWorkers().length - 1,
