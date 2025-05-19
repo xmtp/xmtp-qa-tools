ee7e2a91b1a4722513f8ccae6e3e30c426c681ac
fabrizio guespe <fabrizioguespe@Fabri-M3.local>
2025-05-19 20:04:30 -0300

    Add test for codec error handling in reactions

---
+++ functional/codec-error.test.ts
@@ -0,0 +1,40 @@
+import { loadEnv } from "@helpers/client";
+import { getRandomNames } from "@helpers/tests";
+import { setupTestLifecycle } from "@helpers/vitest";
+import { getWorkers, type WorkerManager } from "@workers/manager";
+import {
+  ContentTypeReaction,
+  type Reaction,
+} from "@xmtp/content-type-reaction";
+import { describe, expect, it } from "vitest";
+
+const testName = "codec-error";
+loadEnv(testName);
+
+describe(testName, async () => {
+  let workers: WorkerManager;
+  workers = await getWorkers(getRandomNames(2), testName);
+
+  setupTestLifecycle({
+    expect,
+  });
+  it("codec-error: should trigger a stream error", async () => {
+    try {
+      const creator = workers.getCreator();
+      const receiver = workers.getReceiver();
+      const convo = await creator.client.conversations.newDm(
+        receiver.client.inboxId,
+      );
+      const reaction: Reaction = {
+        action: "added",
+        content: "smile",
+        reference: "originalMessage",
+        schema: "shortcode",
+      };
+
+      await convo.send(reaction, ContentTypeReaction);
+    } catch (e) {
+      expect(e).toBeDefined();
+    }
+  });
+});
