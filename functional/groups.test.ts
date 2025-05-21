d647359f4fec554dbe193a6875dc2f5eef1e5cb3
fabrizio guespe <fabrizioguespe@Fabri-M3.local>
2025-05-21 12:30:06 -0300

    Add pull request trigger and fix test expectations

--- functional/groups.test.ts
+++ functional/groups.test.ts
@@ -55,7 +55,7 @@ describe(testName, async () => {
       try {
         await groupsBySize[i].sync();
         const members = await groupsBySize[i].members();
-        expect(members.length).toBe(i);
+        expect(members.length).toBe(i + 1);
       } catch (e: unknown) {
         logError(e, expect.getState().currentTestName);
         throw e;
