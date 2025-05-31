34277e3150f634f0dd0a5f4f80c111bc8d7c20ea
fabri <fguespe@gmail.com>
2025-05-31 11:05:03 -0300

    Refactor test suite names for clarity and consistency

--- suites/automated/group/run-tests-periodically.sh
+++ suites/automated/group/run-tests-periodically.sh
@@ -4,7 +4,7 @@ while true; do
     echo "Starting test cycle at $(date)"
     for i in {1..3}; do
         echo "Running test iteration $i of 3"
-        yarn test not-forked --debug-verbose
+        yarn test group --debug-verbose
     done
     echo "Waiting 30 minutes before next cycle..."
     sleep 1800
