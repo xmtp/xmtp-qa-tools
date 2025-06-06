b07496a1e32c6b7c2753069f2f7a01ca4b3cf99c
fabrizio guespe <fabrizioguespe@Fabri-M3.local>
2025-06-05 23:30:14 -0300

    Refactor client logging for improved clarity and efficiency

--- helpers/client.ts
+++ helpers/client.ts
@@ -95,22 +95,22 @@ export const getEncryptionKeyFromHex = (hex: string) => {
 export const logAgentDetails = async (
   clients: Client | Client[],
 ): Promise<void> => {
-  const clientsByAddress = Array.isArray(clients)
-    ? clients.reduce<Record<string, Client[]>>((acc, client) => {
-        const address = client.accountIdentifier?.identifier ?? "";
-        acc[address] = acc[address] ?? [];
-        acc[address].push(client);
-        return acc;
-      }, {})
-    : {
-        [clients.accountIdentifier?.identifier ?? ""]: [clients],
-      };
+  const clientArray = Array.isArray(clients) ? clients : [clients];
+  const clientsByAddress = clientArray.reduce<Record<string, Client[]>>(
+    (acc, client) => {
+      const address = client.accountIdentifier?.identifier as string;
+      acc[address] = acc[address] ?? [];
+      acc[address].push(client);
+      return acc;
+    },
+    {},
+  );
 
   for (const [address, clientGroup] of Object.entries(clientsByAddress)) {
     const firstClient = clientGroup[0];
     const inboxId = firstClient.inboxId;
     const environments = clientGroup
-      .map((c) => c.options?.env ?? "dev")
+      .map((c: Client) => c.options?.env ?? "dev")
       .join(", ");
     console.log(`\x1b[38;2;252;76;52m
         ██╗  ██╗███╗   ███╗████████╗██████╗ 
@@ -124,17 +124,18 @@ export const logAgentDetails = async (
     const urls = [`http://xmtp.chat/dm/${address}`];
 
     const conversations = await firstClient.conversations.list();
+    const installations = await firstClient.preferences.inboxState();
 
     console.log(`
     ✓ XMTP Client:
     • Address: ${address}
+    • Installations: ${installations.installations.length}
     • Conversations: ${conversations.length}
     • InboxId: ${inboxId}
     • Networks: ${environments}
     ${urls.map((url) => `• URL: ${url}`).join("\n")}`);
   }
 };
-
 export const getDbPath = (description: string = "xmtp") => {
   //Checks if the environment is a Railway deployment
   const volumePath = process.env.RAILWAY_VOLUME_MOUNT_PATH ?? ".data/xmtp";
