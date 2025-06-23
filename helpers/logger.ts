a17482523ac5aab4ee4ad67c62b29f88771fe2aa
Fabrizio Guespe <fguespe@gmail.com>
2025-06-23 20:47:34 -0300

    Remove outdated agent test case and update name

--- helpers/logger.ts
+++ helpers/logger.ts
@@ -18,12 +18,6 @@ export const KNOWN_ISSUES = [
     ],
   },
   {
-    testName: "Agents",
-    uniqueErrorLines: [
-      "FAIL  suites/agents/agents.test.ts > agents > production: clankerchat.base.eth : 0x9E73e4126bb22f79f89b6281352d01dd3d203466",
-    ],
-  },
-  {
     testName: "Functional",
     uniqueErrorLines: [
       "FAIL  suites/functional/playwright.test.ts > playwright > conversation stream for new member",
