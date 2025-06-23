a24bad8a5c8d2b49dab0ddfafe1d2a23109e3c34
Fabrizio Guespe <fguespe@gmail.com>
2025-06-23 19:32:14 -0300

    Refactor logger imports for cleaner code structure

--- helpers/logger.ts
+++ helpers/logger.ts
@@ -1,6 +1,6 @@
 import fs from "fs";
 import path from "path";
-import winston, { error } from "winston";
+import winston from "winston";
 import "dotenv/config";
 
 // Known test issues for tracking
