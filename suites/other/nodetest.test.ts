6c3f0ab5b6d54817f1ae5a3985b342da3ad8e49a
Fabrizio Guespe <fguespe@gmail.com>
2025-07-02 18:39:33 -0300

    Add error handling for group creation in tests

--- suites/other/nodetest.test.ts
+++ suites/other/nodetest.test.ts
@@ -75,6 +75,7 @@ const createChaos = async (
   // create test groups
   await Promise.all(
     Array.from({ length: numGroups }).map(async () => {
+      try {
         // create the group
         const group = (await mainClient.conversations.newGroup(
           testClients.map((c) => c.inboxId),
