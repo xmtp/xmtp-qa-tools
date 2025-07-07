22cbdba70e2e7b9d720f29079bfc62ba5b097804
Fabrizio Guespe <fguespe@gmail.com>
2025-07-07 17:23:15 -0300

    Specify node version for worker compatibility

--- suites/bugs/failtowait/test.test.ts
+++ suites/bugs/failtowait/test.test.ts
@@ -9,7 +9,9 @@ const testName = "failtowait";
 describe(testName, async () => {
   setupTestLifecycle({ testName });
 
-  const workers = await getWorkers(1);
+  const workers = await getWorkers(1, {
+    nodeVersion: "3.1.0",
+  });
   const creator = workers.getAll()[0];
 
   it("should create a group with 50 members", async () => {
