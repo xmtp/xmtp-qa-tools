423330e8d3808526a8d13fd776163db2248767b8
Fabrizio Guespe <fguespe@gmail.com>
2025-07-07 23:17:41 -0300

    Update test to reflect correct SDK version for group creation

--- suites/bugs/failtowait/test.test.ts
+++ suites/bugs/failtowait/test.test.ts
@@ -8,7 +8,21 @@ const testName = "failtowait";
 describe(testName, () => {
   // Get 100 inbox IDs for group members
   const memberInboxIds = getInboxIds(100);
-  it("should create a group with 100 members in sdk 3.1.0dev", async () => {
+  it("should create a group with 100 members in sdk 2.0.9", async () => {
+    const workers = await getWorkers(1, {
+      nodeVersion: "2.0.9",
+    });
+    const creator = workers.getAll()[0];
+    console.log(`Creating group with ${memberInboxIds.length} members`);
+
+    // Create the group
+    const group = (await creator.client.conversations.newGroup(
+      memberInboxIds,
+    )) as Group;
+    await group.sync();
+    console.log(`Group created with ID: ${group.id}`);
+  });
+  it("should create a group with 100 members in sdk 3.0.1", async () => {
     const workers = await getWorkers(1, {
       nodeVersion: "3.0.1",
     });
