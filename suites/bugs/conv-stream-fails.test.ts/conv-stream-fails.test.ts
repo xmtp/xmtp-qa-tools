98e6f812538d95eb3ed61cc584fb088091233245
Fabrizio Guespe <fguespe@gmail.com>
2025-07-09 17:24:57 -0300

    Add tests for conversation stream functionality issues

---
+++ suites/bugs/conv-stream-fails.test.ts/conv-stream-fails.test.ts
@@ -0,0 +1,65 @@
+import { playwright } from "@helpers/playwright";
+import { setupTestLifecycle } from "@helpers/vitest";
+import { getInboxIds } from "@inboxes/utils";
+import { typeofStream } from "@workers/main";
+import { getWorkers, type Worker } from "@workers/manager";
+import { beforeAll, describe, expect, it } from "vitest";
+
+const testName = "welcome";
+describe(testName, () => {
+  setupTestLifecycle({ testName });
+  let groupId: string;
+  const headless = false;
+  let xmtpTester: playwright;
+  let creator: Worker;
+  let xmtpChat: Worker;
+
+  beforeAll(async () => {
+    const convoStreamBot = await getWorkers(2);
+    const names = convoStreamBot.getAll().map((w) => w.name);
+    convoStreamBot.startStream(typeofStream.Conversation);
+
+    creator = convoStreamBot.get(names[0]) as Worker;
+    xmtpChat = convoStreamBot.get(names[1]) as Worker;
+    xmtpTester = new playwright({
+      headless,
+      defaultUser: {
+        inboxId: xmtpChat.inboxId,
+        dbEncryptionKey: xmtpChat.encryptionKey,
+        walletKey: xmtpChat.walletKey,
+        accountAddress: xmtpChat.address,
+      },
+    });
+    await xmtpTester.startPage();
+  });
+
+  it("conversation stream adding it as member", async () => {
+    const conversationStream = await creator.client.conversations.stream();
+    groupId = await xmtpTester.newGroupFromUI([...getInboxIds(4)]);
+    await xmtpTester.addMemberToGroup(groupId, creator.inboxId);
+
+    for await (const conversation of conversationStream) {
+      console.log("conversation found", conversation?.id);
+    }
+  }, 10000);
+
+  it("conversation stream when creating the group", async () => {
+    const conversationStream = await creator.client.conversations.stream();
+    await xmtpTester.page?.goto("https://xmtp.chat/conversations/new-group");
+    await xmtpTester.page?.getByRole("button", { name: "Members" }).click();
+
+    const addressInput = xmtpTester.page?.getByRole("textbox", {
+      name: "Address",
+    });
+    for (const address of [...getInboxIds(4), creator.inboxId]) {
+      await addressInput?.fill(address);
+      await xmtpTester.page?.getByRole("button", { name: "Add" }).click();
+    }
+
+    await xmtpTester.page?.getByRole("button", { name: "Create" }).click();
+    for await (const conversation of conversationStream) {
+      console.log("conversation found", conversation?.id);
+      break;
+    }
+  }, 10000);
+});
