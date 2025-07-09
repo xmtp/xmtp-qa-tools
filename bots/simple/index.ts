4ad2aae36f9307de90e09ab08ba3eef7fed7da93
Fabrizio Guespe <fguespe@gmail.com>
2025-07-09 19:32:02 -0300

    Remove unnecessary comment for clarity in code

--- bots/simple/index.ts
+++ bots/simple/index.ts
@@ -32,6 +32,6 @@ const processMessage = async (
 // Initialize the client with the message processor
 await initializeClient(processMessage, [
   {
-    networks: ["production"],
+    networks: ["production"], //sd
   },
 ]);
