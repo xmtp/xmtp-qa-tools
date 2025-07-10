b14a0f9160374df9194da0fbb6136a0f9ccb1839
Isaac Hartford <pianista1337@gmail.com>
2025-07-11 04:45:38 +0700

    Switch back to yarn and debug protocol logging

--- chaossuite-batchrunner.sh
+++ chaossuite-batchrunner.sh
@@ -15,7 +15,7 @@ for ((x=1; x<=tranche_parts; x++)); do
   for ((i=1; i<=tranche; i++)); do
       echo "Restarting singlehost docker env..."
       cd multinode && docker compose down && ./ci.sh && cd ..
-      rm -rf .data/
+      rm -rf .data
 
       sleep 10
       tid=$(date +%s)
@@ -23,8 +23,8 @@ for ((x=1; x<=tranche_parts; x++)); do
 
       #LOGGING_LEVEL="error" LOG_LEVEL="silly" timeout $DURATION_MS npx vitest run suites/networkchaos/forkmatrix.test.ts >& logs/raw-forkmatrix-${tid}.log
       #LOG_LEVEL=info timeout $DURATION_MS time yarn test suites/networkchaos/forkmatrix-streamonly.test.ts --debug
-      #LOGGING_LEVEL="error" LOG_LEVEL="silly" timeout 300 yarn test suites/networkchaos/forkmatrix-streamonly.test.ts --debug
-      LOG_LEVEL=info timeout 300 time npx vitest run suites/networkchaos/forkmatrix-streamonly.test.ts --pool=threads --poolOptions.singleThread=true --fileParallelism=false | tee test-${x}.log
+      LOGGING_LEVEL="error" LOG_LEVEL="silly" timeout 300 yarn test suites/networkchaos/forkmatrix-streamonly.test.ts --debug
+      #LOG_LEVEL=info timeout 300 time npx vitest run suites/networkchaos/forkmatrix-streamonly.test.ts --pool=threads --poolOptions.singleThread=true --fileParallelism=false | tee test-${x}.log
       exit_code=$?
 
       echo "Test iteration $i completed with exit code $exit_code"
