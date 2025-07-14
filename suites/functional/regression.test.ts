d01c07d3d36c9998fdcdc824a792d7e2a6ad02bb
Fabrizio Guespe <fguespe@gmail.com>
2025-07-14 16:45:59 -0300

    Add tests for DM conversation creation and messaging

--- suites/functional/regression.test.ts
+++ suites/functional/regression.test.ts
@@ -1,6 +1,6 @@
 import { getInboxIds } from "@inboxes/utils";
 import { getWorkers, type WorkerManager } from "@workers/manager";
-import { getAutoVersions, getVersions } from "@workers/versions";
+import { getVersions } from "@workers/versions";
 import { describe, expect, it } from "vitest";
 
 describe("regression", () => {
