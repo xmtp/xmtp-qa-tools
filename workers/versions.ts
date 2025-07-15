93384ea18cca7d08803601c36237ffe1a00c9b0d
Fabri <fguespe@gmail.com>
2025-07-15 16:53:03 -0300

    Update to latest release candidates for node bindings (#898)
    Former-commit-id: 22f5c37128976e7815633390d8e0c47444637d35

--- workers/versions.ts
+++ workers/versions.ts
@@ -59,7 +59,7 @@ import {
   Conversation as Conversation320,
   Dm as Dm320,
   Group as Group320,
-} from "@xmtp/node-sdk-3.2.0rc1";
+} from "@xmtp/node-sdk-3.2.0rc2";
 
 // SDK version mappings
 export const VersionList = [
@@ -68,8 +68,8 @@ export const VersionList = [
     Conversation: Conversation320,
     Dm: Dm320,
     Group: Group320,
-    nodeVersion: "3.2.0rc1",
-    bindingsPackage: "1.3.0rc1",
+    nodeVersion: "3.2.0rc2",
+    bindingsPackage: "1.3.0rc2",
     auto: true,
   },
   {
