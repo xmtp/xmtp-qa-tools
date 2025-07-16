0ccc110b1a9bc2905e950c4903f7ef63851838f1
Fabri <fguespe@gmail.com>
2025-07-16 10:16:09 -0300

    Update batch size for performance testing consistency (#919)
    * Update batch size for performance testing consistency
    
    * Add test for adding members to a group in performance suite
    
    Former-commit-id: 101e80d3f4601e76f497d454a54394970e11017c

--- suites/metrics/performance.test.ts
+++ suites/metrics/performance.test.ts
@@ -13,6 +13,7 @@ describe(testName, async () => {
   let dm: Dm | undefined;
   let workers = await getWorkers(10);
 
+  let newGroup: Group;
   const creator = workers.getCreator();
   const creatorClient = creator.client;
   let customDuration: number | undefined = undefined;
@@ -95,72 +96,7 @@ describe(testName, async () => {
     expect(verifyResult.almostAllReceived).toBe(true);
   });
 
-  let i = 4;
-  let newGroup: Group;
-  it(`newGroup: should create a large group of ${i} participants ${i}`, async () => {
-    const sliced = getInboxIds(i);
-    newGroup = (await creatorClient.conversations.newGroup([
-      ...sliced,
-      ...workers.getAll().map((w) => w.client.inboxId),
-    ])) as Group;
-    console.log("New group created", newGroup.id);
-    expect(newGroup.id).toBeDefined();
-  });
-  it(`newGroupByIdentifiers: should create a large group of ${i} participants ${i}`, async () => {
-    const sliced = getAddresses(i);
-    const newGroupByIdentifier =
-      await creatorClient.conversations.newGroupWithIdentifiers(
-        sliced.map((address) => ({
-          identifier: address,
-          identifierKind: IdentifierKind.Ethereum,
-        })),
-      );
-    expect(newGroupByIdentifier.id).toBeDefined();
-  });
-  it(`syncGroup: should sync a large group of ${i} participants ${i}`, async () => {
-    await newGroup.sync();
-    const members = await newGroup.members();
-    expect(members.length).toBe(members.length);
-  });
-  it(`updateGroupName: should update the group name`, async () => {
-    const newName = "Large Group";
-    await newGroup.updateName(newName);
-    await newGroup.sync();
-    const name = newGroup.name;
-    expect(name).toBe(newName);
-  });
-
-  it(`sendGroupMessage: should measure sending a gm in a group of ${i} participants`, async () => {
-    const groupMessage = "gm-" + Math.random().toString(36).substring(2, 15);
-
-    await newGroup.send(groupMessage);
-    expect(groupMessage).toBeDefined();
-  });
-  it(`receiveGroupMessage: should create a group and measure all streams`, async () => {
-    const verifyResult = await verifyMessageStream(
-      newGroup,
-      workers.getAllButCreator(),
-    );
-    setCustomDuration(verifyResult.averageEventTiming);
-    expect(verifyResult.almostAllReceived).toBe(true);
-  });
-  it(`addMembers: should add members to a group`, async () => {
-    await newGroup.addMembers([workers.getAll()[2].inboxId]);
-  });
-  it(`removeMembers: should remove a participant from a group`, async () => {
-    const previousMembers = await newGroup.members();
-    await newGroup.removeMembers([
-      previousMembers.filter(
-        (member) => member.inboxId !== newGroup.addedByInboxId,
-      )[0].inboxId,
-    ]);
-
-    const members = await newGroup.members();
-    expect(members.length).toBe(previousMembers.length - 1);
-  });
-
   for (const i of BATCH_SIZE) {
-    let newGroup: Group;
     it(`newGroup-${i}: should create a large group of ${i} participants ${i}`, async () => {
       const sliced = getInboxIds(i);
       newGroup = (await creatorClient.conversations.newGroup([
@@ -180,7 +116,7 @@ describe(testName, async () => {
         );
       expect(newGroupByIdentifier.id).toBeDefined();
     });
-    it(`syncGroup-${i}: should sync a large group of ${i} participants ${i}`, async () => {
+    it(`sync-${i}: should sync a large group of ${i} participants ${i}`, async () => {
       await newGroup.sync();
       const members = await newGroup.members();
       expect(members.length).toBe(members.length);
@@ -198,13 +134,8 @@ describe(testName, async () => {
       await newGroup.send(groupMessage);
       expect(groupMessage).toBeDefined();
     });
-    it(`receiveGroupMessage-${i}: should create a group and measure all streams`, async () => {
-      const verifyResult = await verifyMessageStream(
-        newGroup,
-        workers.getAllButCreator(),
-      );
-      setCustomDuration(verifyResult.averageEventTiming);
-      expect(verifyResult.almostAllReceived).toBe(true);
+    it(`addMembers-${i}: should add members to a group`, async () => {
+      await newGroup.addMembers([workers.getAll()[2].inboxId]);
     });
     it(`removeMembers-${i}: should remove a participant from a group`, async () => {
       const previousMembers = await newGroup.members();
