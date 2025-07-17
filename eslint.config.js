03f157374a6aa1be9c62d096c212a773daac55b7
Fabri <fguespe@gmail.com>
2025-07-17 20:41:59 -0300

    Refactor client code and improve environment validation (#958)
    * Refactor client code and improve environment validation
    
    * Update @xmtp/node-sdk to improve functionality
    
    * Remove unused import to clean up test file
    
    * bump version to 0.2.18 for new features and fixes
    
    * Update
    
    * Refactor imports and update ESLint rules for clarity
    
    * Fix JSON formatting and add missing newline at EOF
    
    * Refactor README for clarity and basic usage guidance
    
    Former-commit-id: f1247a29c201c1737d66351039391e58133f8f62

--- eslint.config.js
+++ eslint.config.js
@@ -46,6 +46,7 @@ export default tseslint.config(
       "@typescript-eslint/no-unsafe-member-access": "off",
       "@typescript-eslint/no-unsafe-assignment": "off",
       "@typescript-eslint/no-unsafe-call": "off",
+      "@typescript-eslint/no-unsafe-return": "off",
       "@typescript-eslint/unnecessary-type-assertion": "off",
       "@typescript-eslint/restrict-template-expressions": [
         "error",
