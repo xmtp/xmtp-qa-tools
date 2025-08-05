bade0fa461a6da869b6d23863a708355af72a935
Fabri <fguespe@gmail.com>
2025-08-05 14:26:00 -0300

    Refine test configurations for better performance insights (#1240)

--- monitoring/forks/forks.test.ts
+++ monitoring/forks/forks.test.ts
@@ -11,7 +11,7 @@ import { describe, it } from "vitest";
 // Count of groups to create
 const groupCount = 5;
 const parallelOperations = 5; // How many operations to perform in parallel
-const NODE_VERSION = getActiveVersion(); // default to latest version, can be overridden with --nodeSDK=3.1.1
+const NODE_VERSION = getActiveVersion().nodeSDK; // default to latest version, can be overridden with --nodeSDK=3.1.1
 // By calling workers with prefix random1, random2, etc. we guarantee that creates a new key each run
 // We want to create a key each run to ensure the forks are "pure"
 const workerNames = [
