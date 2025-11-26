dbd61478dfe4ca0d5780480659317f14e51a4759
Fabrizio Guespe <fguespe@gmail.com>
2025-11-26 15:13:44 -0300

    Remove sendMessage property

--- agents/helper.ts
+++ agents/helper.ts
@@ -8,10 +8,6 @@ export interface AgentConfig {
   name: string;
   /** Ethereum address */
   address: string;
-  /** Message to send for testing */
-  sendMessage: string;
-  /** Expected response messages (optional) */
-  expectedMessage?: string[];
   /** Networks the agent supports */
   networks: string[];
   /**  the agent is production */
